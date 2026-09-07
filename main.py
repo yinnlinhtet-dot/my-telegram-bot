1e738ec94522986dbbfbda936d544dbe392aae3a
yinnlinhtet-dot <yinnlinhtet@gmail.com>
2026-09-07 18:23:15 +0630

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
-from threading import Thread
+import telebot
 from flask import Flask
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes
+from threading import Thread
+
+# Environment variable ထဲက BOT_TOKEN ကို ယူခြင်း
+TOKEN = os.environ.get("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
 
 app = Flask('')
 
@@ -14,21 +17,13 @@ def run_flask():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot is active!")
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Bot is active!")
 
 if __name__ == '__main__':
-    # Flask ဆာဗာကို Background မှာ Run ရန်
+    # Flask ဆာဗာကို Background မှာ Run ခြင်း
     Thread(target=run_flask).start()
     
-    # Environment variable ထဲက BOT_TOKEN ကို ဖတ်ယူခြင်း
-    TOKEN = os.environ.get("BOT_TOKEN")
-    
-    # Telegram Bot Application ကို တည်ဆောက်ခြင်း
-    application = ApplicationBuilder().token(TOKEN).build()
-    
-    # Start command ထည့်သွင်းခြင်း
-    application.add_handler(CommandHandler("start", start))
-    
-    # Bot ကို စတင် Run ခြင်း
-    application.run_polling()
+    # Telegram Bot ကို စတင် Run ခြင်း
+    bot.infinity_polling()
